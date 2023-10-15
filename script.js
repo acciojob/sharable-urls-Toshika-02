bb59edb406eaf438f3378318b748608e93506bcc
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2023-10-16 01:46:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 // your code here
+function Submit() {
+            const name = document.getElementById("name").value;
+            const year = document.getElementById("year").value;
+            const url = `https://localhost:8080/?name=${name}&year=${year}`;
+            document.getElementById("url").textContent = url;
+        }
 
