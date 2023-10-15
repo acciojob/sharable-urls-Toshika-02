f59df70309df13fbd77d2d94395ac1a9dd272f33
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2023-10-16 01:37:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
 // your code here
-function Submit() {
-            const name = document.getElementById("name").value;
-            const year = document.getElementById("year").value;
-            const url = `https://localhost:8080/?name=${name}&year=${year}`;
-            document.getElementById("url").textContent = url;
+
